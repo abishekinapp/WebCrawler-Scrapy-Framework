c418cde1d00500947715243b0f95f80fee76e203
abishek <abishek@inapp.com>
2018-09-05 10:43:37 +0530

    installer.sh optimized

--- Web_crawlers/hindu/installer.sh
+++ Web_crawlers/hindu/installer.sh
@@ -1,11 +1,23 @@
+
 #!/bin/bash
 
+#Installing virtualenv
+python -m pip install --user virtualenv
+#Creaeting virtualenv
+python -m virtualenv env
+#Activating virtualenv
+source env/bin/activate
+#Installing twisted for scrapy
+pip install twisted
+#Installing scrapy framework
+pip install scrapy
+
+var_path=$PWD # storing the curent working directory for future use
 cd /etc
 source ./bash.bashrc
-source /usr/local/bin/virtualenvwrapper.sh #opening virstualenv for running scrapy
 
-#directory of the project
-cd /home/inapp/abishek_Training/phase_3/Web_crawlers/hindu 
+#Redirecting to the Project for Running the spider
+cd $var_path
 
-#command to start crawling the spider
+#command for start Crawling
 scrapy crawl crawl_spider
